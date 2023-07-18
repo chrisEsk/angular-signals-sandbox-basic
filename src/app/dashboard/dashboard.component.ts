de83342845a85dbfa43bf8be5673b57e20f3cbe3
Christian Esquivel <chrisesk.dev@gmail.com>
2023-07-17 19:02:09 -0600

    rn

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -27,8 +27,8 @@ export class DashboardComponent implements OnInit {
   }
 
   aggregateGBPPrice() {
-    this.products.mutate(product => {
-      product.forEach(e => {
+    this.products.mutate(products => {
+      products.forEach(e => {
         e.price_gbp = e.price_usd ? e.price_usd * this.usd_gbp_exchangeRate : 0;
       });
     });
